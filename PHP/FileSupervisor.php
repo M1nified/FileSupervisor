0c74b9455c5dfddb5ea2a7b85ee948acd7246b82
Minified <gora.pub@gmail.com>
2016-01-17 11:39:11 +0100

    Protected fields

--- PHP/FileSupervisor.php
+++ PHP/FileSupervisor.php
@@ -1,43 +1,37 @@
 <?php
 /**
-*
+* FileSupervisor Class
 */
 class FileSupervisor
 {
+  protected $statusFileName = NULL;
+  protected $fileNameRegExps = NULL;
+  protected $checkResult = [];
+  protected $stats = [];
 
-  function __construct($statusFile,$filesRegExps)
+  function __construct($statusFileName,$fileNameRegExps)
   {
-    // echo 'STATUS FILE IS '.$statusFile;
-    $this -> statusFile = $statusFile;
-    $this -> filesRegExps = $filesRegExps;
+    $this -> statusFileName = $statusFileName;
+    $this -> fileNameRegExps = $fileNameRegExps;
     $this -> checkResult = [];
     $this -> stats = [];
     $stats = [];
-    if (($statsHandler = fopen ($this -> statusFile,"r")) !== FALSE) {
-      // throw new Exception("Error while accessing stats file", 1);
+    if (($statsHandler = fopen ($this -> statusFileName,"r")) !== FALSE) {
       while(!feof($statsHandler)){
         $stats[] = fgetcsv($statsHandler);
       }
+      fclose($statsHandler);
     }
-    fclose($statsHandler);
-    // echo "READ CSV";
-    // print_r($stats);
     foreach ($stats as $row) {
-      // echo $row;
       $this -> stats[$row[0]] = $row;
     }
-    // echo "##### STATS BELOW: #####\n";
-    // print_r($this -> stats);
   }
   function runCheck(){
-    // echo "\n";
-    foreach ($this -> filesRegExps as $fileRegExp) {
-      // echo "CHECKING ".$fileRegExp;
+    foreach ($this -> fileNameRegExps as $fileRegExp) {
       $matchedFiles = glob($fileRegExp);
-      // print_r($matchedFiles);
       $this -> checkFilesByName($matchedFiles);
     }
-    $statsHandler = fopen($this -> statusFile,"w+");
+    $statsHandler = fopen($this -> statusFileName,"w+");
     foreach ($this -> stats as $key => $line){
       $record = explode(',',$line);
       array_unshift($record,$key);
@@ -61,16 +55,10 @@ class FileSupervisor
     }
   }
   private function checkChanges($filePath){
-    // print_r("## CHECKING SINGLE FILE: {$filePath} \n");
-    // $md5_old = "";
-    // try {
     @$md5_old = $this -> stats[$filePath][1];
     var_dump($md5_old);
-    // } catch (Exception $e) {}
     $md5_new = md5_file($filePath);
     $this -> stats[$filePath] = $md5_new;
-    // print_r("STATS (OLD|NEW): {$md5_old}|{$md5_new}");
-    // print_r("\n");
     if($md5_old === NULL){
       return 2;//added
     }
@@ -82,12 +70,9 @@ class FileSupervisor
   public function resultToFile($fname = 'result.csv'){
     $fhandle = fopen($fname,"w+");
     foreach ($this -> checkResult as $key => $line){
-      // $record = explode(',',$line);
-      // array_unshift($record,$key);
       fputcsv($fhandle,$line);
     }
     fclose($fhandle);
   }
 }
-
 ?>
